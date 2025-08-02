084a7c4780f33f09127fbd4fa1973190af3ac151
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-08-02 11:46:22 +0100

    update changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const openMenu = document.querySelector(".nav__btn");
 const navBar = document.querySelector(".nav__menu");
-const navItem = document.querySelectorAll(".nav__item");
+const navItem = document.querySelectorAll(".nav__link");
 
 openMenu.addEventListener("click", () => {
     navBar.classList.toggle("toggle");
@@ -10,6 +10,7 @@ navItem.forEach(item => {
     item.addEventListener("click", () => {
         navItem.forEach(nav => nav.classList.remove("active"));
         item.classList.add("active");
+        
         navBar.classList.remove("toggle");
     })
 });
@@ -21,7 +22,9 @@ toggleBtn.forEach(btn =>{
     btn.addEventListener("click", () => {
         const container = btn.closest(".course__container");
         const toggle = container.querySelector(".course__toggle");
+        const arrow = btn.querySelector(".course__arrow");
         toggle.classList.toggle("toggle");
+        arrow.classList.toggle("rotated");
     });
 });
 
